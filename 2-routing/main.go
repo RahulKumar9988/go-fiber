f974953d9a77a4871b71c21889e2fedb5bb42c55
rahul-rj-battlebucks <stkwhy000@gmail.com>
2025-12-23 22:19:08 +0530

    props , params, etc

--- 2-routing/main.go
+++ 2-routing/main.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
+
+type Person struct {
+	Name string `json:"name" db:"namae"`
+	Age  int    `json:"age" db:"age"`
+}
+
+type ErrorResponse struct {
+	Message string
+	Status  string
+}
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("2 leaning about routing")
+		return c.SendString("leaning about routing")
 	})
 
-	// Grouping Routes
-	app.Route("/v1/users", func(api fiber.Router) {
-		api.Get("/", handler)
-		api.Post("/", handler)
-		api.Patch("/:id", handler)
-		api.Delete("/:id", handler)
+	app.Get("/store/:storeName/:product", func(c *fiber.Ctx) error {
+		storeName := c.Params("storeName")
+		product := c.Params("product")
+
+		fmt.Println(storeName, " + ", product)
+
+		return c.SendStatus(fiber.StatusOK)
 	})
 
-	app.Server().MaxConnsPerIP = 1
+	app.Post("/pars", func(c *fiber.Ctx) error {
+		p := new(Person)
+		if err := c.BodyParser(p); err != nil {
+			c.Locals("message : ", "Error found", err)
+			return c.Status(fiber.StatusBadRequest).JSON(ErrorResponse{
+				Message: "Pass the body",
+				Status:  "400",
+			})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(Person{
+			Name: p.Name,
+			Age:  p.Age,
+		})
+
+	})
+	app.Server().MaxConnsPerIP = 2
 	app.Listen(":3000")
 }
